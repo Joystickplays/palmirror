484f69bc9e43d0a8eb9c866a1b33721b069a6629
Joystickplays <joystickgaming09@gmail.com>
2024-11-06 21:44:30 +0700

    This commit introduces a new UI component library and theming solution.  Several new components have been added, including Avatar, Button, and Input.  Theming is handled via a new ThemeProvider component. Main page layout has been changed.   Unnecessary SVG files have been removed.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>PalMirror</h1>
+    <div className="grid items-center justify-items-center content-center min-h-screen p-8 pb-20 gap-12 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        PalMirror
+      </h1>
+
+      <div className="flex flex-row gap-6">
+        <Input placeholder="JanitorAI character link" />
+        <Button>Start</Button>  
+      </div>
     </div>
   );
 }
