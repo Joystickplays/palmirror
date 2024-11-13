87c73b777cedd195d5fbba773effd107e9f8e1cc
Joystick <joystickgaming09@gmail.com>
2024-11-13 07:27:17 +0700

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -90,6 +90,7 @@ const ChatPage = () => {
         abortController.current = new AbortController();
 
         try {
+          console.log(messages)
           const comp = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [
