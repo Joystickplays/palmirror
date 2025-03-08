de6c4fd1fac444f6457f27315fcb9298dccc7db8
Joystick <joystickgaming09@gmail.com>
2025-03-09 01:07:39 +0700

    pin unlock

--- app/secure/page.tsx
+++ app/secure/page.tsx
@@ -1,185 +1,311 @@
-"use client"
+"use client";
 
-import { useState, useEffect, useRef } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { ToastContainer, toast } from 'react-toastify';
-import { Check } from 'lucide-react';
-import 'react-toastify/dist/ReactToastify.css';
-
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    DialogClose,
-} from "@/components/ui/dialog"
+import { useState, useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ToastContainer, toast } from "react-toastify";
+import { Check } from "lucide-react";
+import Keypad from "@/components/Keypad";
+import PinDisplay from "@/components/PINDisplay";
+import "react-toastify/dist/ReactToastify.css";
 
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
 
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-import { setSecureData, removeSecureData, isPalMirrorSecureActivated } from '@/utils/palMirrorSecureUtils';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
-import { useRouter } from 'next/navigation';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  setSecureData,
+  removeSecureData,
+  isPalMirrorSecureActivated,
+} from "@/utils/palMirrorSecureUtils";
+
+import { useRouter } from "next/navigation";
+
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Home() {
+  const router = useRouter();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [pin, setPin] = useState("");
+  const [showPin, setShowPin] = useState(false);
+  
+  const [selectedMethod, setSelectedMethod] = useState("password");
 
-    const router = useRouter();
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [alreadyEncrypted, setAlreadyEncrypted] = useState(false);
+  const [alreadyEncrypted, setAlreadyEncrypted] = useState(false);
 
-    const setupPLMSecure = async () => {
-        if (password.length < 6) {
-            toast.error('Password too short!');
-            return;
-        }
-        if (password !== confirmPassword) {
-            toast.error('Passwords do not match!');
-            return;
-        }
+  const handleKeyPressPin = (key: string) => {
+    if (key === "⌫") {
+      setPin((prev) => prev.slice(0, -1));
+    } else if (pin.length < 12) {
+      setPin((prev) => prev + key);
+    }
+  };
 
-        try {
-            await setSecureData('generalSettings', {
-                proxy: {
-                    api_key: ""
-                }
-            }, password);
-            toast.success('Setup successful!');
-            setAlreadyEncrypted(true);
-        } catch (error) {
-            toast.error('Failed to setup PalMirror Secure...');
-            console.log(error)
-        }
-        setPassword("")
-        setConfirmPassword("")
+  const setupPLMSecure = async () => {
+    if (selectedMethod === "password" && password.length < 6) {
+      toast.error("Password too short!");
+      return;
+    }
+    if (selectedMethod === "pin" && pin.length < 6) {
+      toast.error("PIN too short! Atleast 6 digits.");
+      return;
+    }
+    if (selectedMethod === "password" && password !== confirmPassword) {
+      toast.error("Passwords do not match!");
+      return;
+    }
+
+    try {
+      await setSecureData(
+        "generalSettings",
+        {
+          proxy: {
+            api_key: "",
+          },
+        },
+        selectedMethod === "password" ? password : pin,
+      );
+      if (selectedMethod === "pin") {
+        localStorage.setItem("secureMetadata", JSON.stringify({ type: selectedMethod, length: pin.length }))
+      }
+      toast.success("Setup successful!");
+      setAlreadyEncrypted(true);
+    } catch (error) {
+      toast.error("Failed to setup PalMirror Secure...");
+      console.log(error);
+    }
+    setPassword("");
+    setConfirmPassword("");
+    setPin("");
+  };
+
+  const removePLMSecure = async () => {
+    await indexedDB.deleteDatabase("PalMirrorSecure");
+    localStorage.removeItem("secureMetadata")
+    setAlreadyEncrypted(false);
+    toast.success("PalMirror Secure removed successfully!");
+  };
+
+  useEffect(() => {
+    const checkEncryptionStatus = async () => {
+      try {
+        const activated = await isPalMirrorSecureActivated();
+        setAlreadyEncrypted(activated);
+      } catch (error) {
+        console.error("Failed to check encryption status:", error);
+      }
     };
 
-    const removePLMSecure = async () => {
-        await indexedDB.deleteDatabase('PalMirrorSecure');
-        setAlreadyEncrypted(false);
-        toast.success('PalMirror Secure removed successfully!');
-    };
+    checkEncryptionStatus();
+    console.log(alreadyEncrypted);
+  }, []);
 
-    useEffect(() => {
-        const checkEncryptionStatus = async () => {
-            try {
-                const activated = await isPalMirrorSecureActivated();
-                setAlreadyEncrypted(activated);
-            } catch (error) {
-                console.error('Failed to check encryption status:', error);
-            }
-        };
+  return (
+    <div className="grid items-center justify-items-center content-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="scroll-m-20 text-1xl font-extrabold tracking-tight pb-2">
+        PalMirror Secure
+      </h1>
+      <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl pb-2 text-center w-4/5">
+        Your chats is yours, and yours only.
+      </h1>
+      {!alreadyEncrypted && (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Setup PalMirror Secure</Button>
+          </DialogTrigger>
+          <DialogContent className="font-sans">
+            <DialogHeader>
+              <DialogTitle>Setup PalMirror Secure</DialogTitle>
+            </DialogHeader>
+            <Tabs defaultValue="password" className="block mx-auto w-full" value={selectedMethod} onValueChange={setSelectedMethod}>
+              <TabsList className="w-full mb-2">
+                <TabsTrigger className="w-full" value="password">
+                  Password
+                </TabsTrigger>
+                <TabsTrigger className="w-full" value="pin">
+                  PIN
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="password" asChild>
+                <motion.div
+                  key="password"
+                  className="flex flex-col gap-4"
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    mass: 1,
+                    stiffness: 250,
+                    damping: 22,
+                  }}
+                >
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Label htmlFor="confirmPassword">Confirm Password</Label>
+                  <Input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="Confirm your password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                  <DialogClose asChild>
+                    <Button onClick={setupPLMSecure}>Encrypt</Button>
+                  </DialogClose>
+                  <p className="text-sm opacity-70 text-red-500 text-center">
+                    PalMirror Secure is NOT recoverable! If you forget the
+                    password, your chats will need to be wiped.
+                  </p>
+                </motion.div>
+              </TabsContent>
+              <TabsContent value="pin" asChild>
+                <motion.div
+                  key="pin"
+                  className="flex flex-col gap-4"
+                  initial={{ scale: 0.8, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    mass: 1,
+                    stiffness: 250,
+                    damping: 22,
+                  }}
+                >
+                  <PinDisplay input={pin} show={showPin} />
+                  <Keypad onKeyPress={handleKeyPressPin} />
+                  <div className="flex gap-2 justify-end">
+                    <DialogClose asChild>
+                      <Button className="w-full" onClick={setupPLMSecure}>
+                        Encrypt
+                      </Button>
+                    </DialogClose>
+                    <Button
+                      variant="outline"
+                      onClick={() => {
+                        setShowPin(!showPin);
+                      }}
+                    >
+                      {showPin ? "Hide" : "Show"} PIN
+                    </Button>
+                  </div>
 
-        checkEncryptionStatus();
-        console.log(alreadyEncrypted);
-    }, []);
-
-    return (
-        <div className="grid items-center justify-items-center content-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <h1 className="scroll-m-20 text-1xl font-extrabold tracking-tight pb-2">
-                PalMirror Secure
-            </h1>
-            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-5xl pb-2 text-center w-4/5">
-                Your chats is yours, and yours only.
-            </h1>
-            {!alreadyEncrypted && (
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <Button>Setup PalMirror Secure</Button>
-                    </DialogTrigger>
-                    <DialogContent className="font-sans">
-                        <DialogHeader>
-                            <DialogTitle>Setup PalMirror Secure</DialogTitle>
-                        </DialogHeader>
-                        <div className="flex flex-col gap-4">
-                            <Label htmlFor="password">Password</Label>
-                            <Input
-                                type="password"
-                                id="password"
-                                placeholder="Enter your password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <Label htmlFor="confirmPassword">Confirm Password</Label>
-                            <Input
-                                type="password"
-                                id="confirmPassword"
-                                placeholder="Confirm your password"
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                            />
-                            <DialogClose asChild>
-                                <Button onClick={setupPLMSecure}>Encrypt</Button>
-                            </DialogClose>
-                            <p className="text-sm opacity-70 text-red-500 text-center">PalMirror Secure is NOT recoverable! If you forget the password, your chats will need to be wiped.</p>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-            )}
-            {alreadyEncrypted && (
-                <div className="flex flex-col gap-2">
-                    <p className="text-sm">Woo! You&apos;re already using PalMirror Secure.</p>
-                    <Dialog>
-                        <DialogTrigger asChild>
-                            <Button>Remove PalMirror Secure</Button>
-                        </DialogTrigger>
-                        <DialogContent className="font-sans">
-                            <DialogHeader>
-                                <DialogTitle>Remove PalMirror Secure</DialogTitle>
-                            </DialogHeader>
-                            <div className="flex flex-col gap-4">
-                                <p>By removing PalMirror Secure, lost data will include:</p>
-                                <ul className="list-disc pl-5">
-                                    <li>Encrypted chats</li>
-                                    <li>API Key configuration</li>
-                                </ul>
-                                <p>Are you sure you want to remove PalMirror Secure? This action cannot be undone. </p>
-                                <DialogClose asChild>
-                                    <Button variant="destructive" onClick={removePLMSecure}>Confirm Removal</Button>
-                                </DialogClose>
-                            </div>
-                        </DialogContent>
-                    </Dialog>
-                </div>
-            )}
-           
-            <Accordion type="single" collapsible className="w-full mb-4 mx-4 lg:mx-20">
-                <AccordionItem value="item-1">
-                    <AccordionTrigger>Why use PalMirror Secure?</AccordionTrigger>
-                    <AccordionContent className="flex flex-col gap-2">
-                        <p>By using PalMirror Secure, your chats can be securely saved locally on the device with encryption. You no longer have to continuously export and import chats.</p>
-                        <ul className="list-disc pl-5">
-                            <li className="flex gap-2"><Check /> Local storage of chats</li>
-                            <li className="flex gap-2"><Check /> Enhanced privacy with encryption</li>
-                            <li className="flex gap-2"><Check /> Requires a password to unlock your chats</li>
-                            <li className="flex gap-2"><Check /> Easy setup</li>
-                        </ul>
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                theme="dark"
-            />
+                  <p className="text-sm opacity-70 text-red-500 text-center">
+                    PINs are easier to crack than passwords. Use a long, secure
+                    PIN and avoid common patterns like &apos;0000&apos;,
+                    &apos;1234&apos;, or your birthday.
+                  </p>
+                  <p className="text-sm opacity-70 text-red-500 text-center">
+                    PalMirror Secure is NOT recoverable! If you forget the
+                    password, your chats will need to be wiped.
+                  </p>
+                </motion.div>
+              </TabsContent>
+            </Tabs>
+          </DialogContent>
+        </Dialog>
+      )}
+      {alreadyEncrypted && (
+        <div className="flex flex-col gap-2">
+          <p className="text-sm">
+            Woo! You&apos;re already using PalMirror Secure.
+          </p>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Remove PalMirror Secure</Button>
+            </DialogTrigger>
+            <DialogContent className="font-sans">
+              <DialogHeader>
+                <DialogTitle>Remove PalMirror Secure</DialogTitle>
+              </DialogHeader>
+              <div className="flex flex-col gap-4">
+                <p>By removing PalMirror Secure, lost data will include:</p>
+                <ul className="list-disc pl-5">
+                  <li>Encrypted chats</li>
+                  <li>API Key configuration</li>
+                </ul>
+                <p>
+                  Are you sure you want to remove PalMirror Secure? This action
+                  cannot be undone.{" "}
+                </p>
+                <DialogClose asChild>
+                  <Button variant="destructive" onClick={removePLMSecure}>
+                    Confirm Removal
+                  </Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
-    );
+      )}
+
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full mb-4 mx-4 lg:mx-20"
+      >
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Why use PalMirror Secure?</AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-2">
+            <p>
+              By using PalMirror Secure, your chats can be securely saved
+              locally on the device with encryption. You no longer have to
+              continuously export and import chats.
+            </p>
+            <ul className="list-disc pl-5">
+              <li className="flex gap-2">
+                <Check /> Local storage of chats
+              </li>
+              <li className="flex gap-2">
+                <Check /> Enhanced privacy with encryption
+              </li>
+              <li className="flex gap-2">
+                <Check /> Requires a password to unlock your chats
+              </li>
+              <li className="flex gap-2">
+                <Check /> Easy setup
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="dark"
+      />
+    </div>
+  );
 }
