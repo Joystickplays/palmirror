bd068a3a383d4c8fbbcbfe6491a8df5b9440428e
Joystickplays <joystickgaming09@gmail.com>
2025-11-15 11:36:42 +0700

    hide steer for now

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -180,12 +180,12 @@ const MessageInput: React.FC<MessageInputProps> = ({
             </span>
           </ContextMenuItem>
 
-          <ContextMenuItem onClick={showSteerModal}>
+          {/* <ContextMenuItem onClick={showSteerModal}>
             <span className="flex items-center gap-2">
               <ShipWheel className="h-4 w-4" />
               Steer the story
             </span>
-          </ContextMenuItem>
+          </ContextMenuItem> */}
         </ContextMenuContent>
       </ContextMenu>
     </div>
