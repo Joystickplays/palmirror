00716cc15479938d0f40ee8391b9305d061d0695
Joystickplays <joystickgaming09@gmail.com>
2026-01-26 15:08:53 +0800

    feat: sidebar content with buttons

--- components/homescreen/Sidebar.tsx
+++ components/homescreen/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { useSidebarStore } from "@/context/zustandStore/Sidebar";
 import { motion } from "framer-motion";
+import { Compass, MessageCircle, Plus, Settings } from "lucide-react";
 
 
 export default function Sidebar() {
@@ -10,11 +11,29 @@ export default function Sidebar() {
     return (<>
     <button onClick={() => setOpen(!isOpen)} className="fixed top-4 right-4 z-51">toggle sidebar</button>
       <motion.div
-        initial={{ x: -300 }}
-        animate={{ x: isOpen ? 0 : -300 }}
-        transition={{ type: "spring", stiffness: 300, damping: 30 }}
-        className="fixed top-0 left-0 h-full w-12 bg-white/5 border border-white/5 text-white z-50">
+        initial={{ x: '-100%' }}
+        animate={{ x: isOpen ? 0 : '-100%' }}
+        transition={{ type: "spring", stiffness: 160, damping: 20 }}
+        className="flex flex-col items-center gap-2 fixed top-0 left-0 h-full bg-white/2 border border-white/5 text-white z-50 p-6 backdrop-blur-xl">
         
+        <img src="./palmirror.png" width={64} />
+        <div className="w-full h-0.5 bg-white/5 mb-6"></div>
+
+        <motion.button className="rounded-full bg-white/5 border border-white/5 p-3">
+            <Plus size={32} />
+        </motion.button>
+
+        <div className="flex flex-col gap-2 h-full justify-center">
+            <button className="bg-white/5 p-4 rounded-2xl">
+                <Compass />
+            </button>
+            <button className="bg-white/5 p-4 rounded-2xl">
+                <MessageCircle />
+            </button>
+            <button className="bg-white/5 p-4 rounded-2xl">
+                <Settings />
+            </button>
+        </div>
 
 
     </motion.div>  
