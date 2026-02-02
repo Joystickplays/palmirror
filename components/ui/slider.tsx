2b7e4d3d244e4763b800ceb22de947716a7d46ce
Joystickplays <joystickgaming09@gmail.com>
2026-02-02 15:29:35 +0800

    feat: slider pull effect

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -4,24 +4,34 @@ import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "@/lib/utils"
+import { motion } from "framer-motion"
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
+  <motion.div
     className={cn(
-      "relative flex w-full touch-none select-none items-center",
+      "relative w-full touch-none select-none items-center",
       className
     )}
-    {...props}
+    drag={"x"}
+    dragConstraints={{ left: 0, right: 0 }}
+    dragElastic={0.03}
+
+    dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
   >
-    <SliderPrimitive.Track className="relative h-4 w-full grow overflow-hidden rounded-full bg-secondary">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-6 w-2 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-  </SliderPrimitive.Root>
+    <SliderPrimitive.Root
+      ref={ref}
+      className="relative flex h-5 w-full select-none items-center"
+      {...props}
+    >
+      <SliderPrimitive.Track className="relative h-4 w-full grow overflow-hidden rounded-full bg-secondary">
+        <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      </SliderPrimitive.Track>
+      <SliderPrimitive.Thumb className="block h-6 w-2 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    </SliderPrimitive.Root>
+  </motion.div>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
 
