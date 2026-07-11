c3f39376bdd5f8b2202645a7bfbfc1e98c311f7e
Joystickplays <joystickgaming09@gmail.com>
2026-07-11 19:31:14 +0700

    fix: annoying no space

--- components/utilities/TokenCounter.tsx
+++ components/utilities/TokenCounter.tsx
@@ -20,7 +20,7 @@ const TokenCounter: React.FC<TokenCounterProps> = ({
 
   return (
     <p className="text-[0.3rem] opacity-50 font-mono absolute top-0 left-1/2 transform -translate-x-1/2 p-2 z-9999">
-      <NumberFlow value={tokenCount}></NumberFlow>tokens
+      <NumberFlow value={tokenCount}></NumberFlow> tokens
     </p>
   );
 };
